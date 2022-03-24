08da3b696b649e786755a0f73ddc842d7c28049e
AnaCarolina12 <76164929+AnaCarolina12@users.noreply.github.com>
2022-03-24 09:49:47 +0100

    Empleados

---
+++ java/com/example/Listado/ListadoApplication.java
@@ -0,0 +1,13 @@
+package com.example.Listado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListadoApplication.class, args);
+	}
+
+}
