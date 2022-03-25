026aeafeac45f4ed22da4f4f32a773b541dc1bca
AnaCarolina12 <76164929+AnaCarolina12@users.noreply.github.com>
2022-03-25 12:29:00 +0100

    EmpleadosRest

---
+++ java/com/example/Empleados/controller/EmpleadosController.java
@@ -0,0 +1,49 @@
+package com.example.Empleados.controller;
+
+import com.example.Empleados.models.EmpleadosModel;
+import com.example.Empleados.services.EmpleadosService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+public class EmpleadosController {
+
+    @Autowired
+private EmpleadosService empleadosService;
+
+   @RequestMapping("/empleados")
+    public List<EmpleadosModel> getAllEmpleados(){
+
+    return  empleadosService.getAllEmpleados();
+   }
+
+
+   @GetMapping("empleados/{dni}")
+    public EmpleadosModel getEmpleado(@PathVariable String dni){
+
+       return empleadosService.getEmpleados(dni);
+   }
+
+
+   @PostMapping("/empleados")
+    public void addEmpleados(@RequestBody EmpleadosModel empleadosModel){
+
+       empleadosService.addEmpleados(empleadosModel);
+   }
+
+   @PutMapping("/empleados/{id}")
+    public  void updateEmpleados( @PathVariable Integer id,@RequestBody EmpleadosModel empleadosModel) {
+
+       empleadosService.updateEmpleado(id,empleadosModel);
+   }
+
+
+    @DeleteMapping("/empleados/{id}")
+  public void deleteEmpelados(@PathVariable Integer id)  {
+
+       empleadosService.deleteEmpleados(id);
+    }
+}
