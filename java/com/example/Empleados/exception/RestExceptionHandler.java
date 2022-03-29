cb5bc29c9b8a972006b20fbb2bf8d740990f86cc
AnaCarolina12 <76164929+AnaCarolina12@users.noreply.github.com>
2022-03-29 15:59:21 +0200

    Empleados

---
+++ java/com/example/Empleados/exception/RestExceptionHandler.java
@@ -0,0 +1,43 @@
+package com.example.Empleados.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class RestExceptionHandler {
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorObject> handlerFieldEmptyException(FieldEmptyException e){
+        ErrorObject errorObject= new ErrorObject();
+        errorObject.setMessage(e.getMessage());
+        errorObject.setStatusCode(HttpStatus.NO_CONTENT.value());
+        errorObject.setHttpStatus(HttpStatus.NO_CONTENT);
+
+        return new ResponseEntity<>(errorObject,HttpStatus.OK);
+
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorObject> handlerNoDataFoundException(NoDataFoundException e){
+        ErrorObject errorObject= new ErrorObject();
+        errorObject.setMessage(e.getMessage());
+        errorObject.setStatusCode(HttpStatus.NOT_FOUND.value());
+        errorObject.setHttpStatus(HttpStatus.NOT_FOUND);
+
+        return new ResponseEntity<>(errorObject,HttpStatus.NOT_FOUND);
+
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorObject> handlerRescourceNotFoundException(RescourceNotFoundException e){
+        ErrorObject errorObject= new ErrorObject();
+        errorObject.setMessage(e.getMessage());
+        errorObject.setStatusCode(HttpStatus.BAD_REQUEST.value());
+        errorObject.setHttpStatus(HttpStatus.BAD_REQUEST);
+
+        return new ResponseEntity<>(errorObject,HttpStatus.BAD_REQUEST);
+
+    }
+}
