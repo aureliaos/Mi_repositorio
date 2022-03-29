da59832478eab6f4c1f6f30cc41417e170303e53
AnaCarolina12 <76164929+AnaCarolina12@users.noreply.github.com>
2022-03-29 16:02:15 +0200

    Add files via upload

---
+++ java/com/example/Empleados/exception/RescourceNotFoundException.java
@@ -0,0 +1,9 @@
+package com.example.Empleados.exception;
+
+public class RescourceNotFoundException extends  RuntimeException{
+
+    public RescourceNotFoundException(String message) {
+        super(message);
+    }
+
+}
